d041df1ba89425a6255f6dcb0c800f60af2f37b2
Carlos Bruguera <cbruguera@gmail.com>
2017-10-13 04:41:14 -0400

    Token amounts updated.

--- ethereum/contracts/CrowdsaleConfig.sol
+++ ethereum/contracts/CrowdsaleConfig.sol
@@ -9,12 +9,12 @@ contract CrowdsaleConfig {
     // Initial distribution amounts
     uint256 public constant TOKEN_DECIMALS = 18;
 
-    uint256 public constant TOTAL_SUPPLY_CAP = 99000000000 * (10 ** uint256(TOKEN_DECIMALS));
+    uint256 public constant TOTAL_SUPPLY_CAP = 9999999000 * (10 ** uint256(TOKEN_DECIMALS));
 
-    uint256 public constant FOUNDATION_POOL_TOKENS = 3267000000 * (10 ** uint256(TOKEN_DECIMALS));   // 33%
-    uint256 public constant TIMELOCK1_TOKENS = 3267000000 * (10 ** uint256(TOKEN_DECIMALS));         // 33%
-    uint256 public constant LEGAL_EXPENSES_TOKENS = 99000000 * (10 ** uint256(TOKEN_DECIMALS));      //  1%
+    uint256 public constant FOUNDATION_POOL_TOKENS = 3299999670 * (10 ** uint256(TOKEN_DECIMALS));   // 33%
+    uint256 public constant TIMELOCK1_TOKENS = 3299999670 * (10 ** uint256(TOKEN_DECIMALS));         // 33%
+    uint256 public constant LEGAL_EXPENSES_TOKENS = 99999990 * (10 ** uint256(TOKEN_DECIMALS));      //  1%
 
-    uint256 public constant PRESALE_CAP = 1320000000 * (10 ** uint256(TOKEN_DECIMALS));
-    uint256 public constant SALE_CAP = 3267000000 * (10 ** uint256(TOKEN_DECIMALS));                 // 33%
+    uint256 public constant PRESALE_CAP = 2639999736 * (10 ** uint256(TOKEN_DECIMALS));              // 80% of the sale cap
+    uint256 public constant SALE_CAP = 3299999670 * (10 ** uint256(TOKEN_DECIMALS));                 // 33%
 }
