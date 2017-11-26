31cb747cb951c769801c1ff8004e50bc9dd823b8
Carlos Bruguera <cbruguera@gmail.com>
2017-11-26 17:10:57 -0400

    KEY-21 Updated caps and rates

--- contracts/CrowdsaleConfig.sol
+++ contracts/CrowdsaleConfig.sol
@@ -7,29 +7,29 @@ pragma solidity ^0.4.18;
 */
 contract CrowdsaleConfig {
     uint256 private constant TOKEN_DECIMALS = 18;
-    uint256 private constant QUINTILLION = 10 ** uint256(TOKEN_DECIMALS);
+    uint256 private constant MIN_UNIT = 10 ** uint256(TOKEN_DECIMALS);
 
     // Initial distribution amounts
-    uint256 public constant TOTAL_SUPPLY_CAP = 9999999000 * QUINTILLION;
+    uint256 public constant TOTAL_SUPPLY_CAP = 6000000000 * MIN_UNIT;
 
-    // approx 26.4% of the sale cap
-    uint256 public constant PRESALE_CAP = 2639999736 * QUINTILLION;
+    // 33% of the total supply cap
+    uint256 public constant SALE_CAP = 1980000000 * MIN_UNIT;
 
-    // approx 33%
-    uint256 public constant SALE_CAP = 3299999670 * QUINTILLION;
+    // 80% of the sale cap
+    uint256 public constant PRESALE_CAP = 1584000000 * MIN_UNIT;
 
-    // approx. $100 in wei
-    uint256 public constant PURCHASE_MIN_CAP_WEI = 333333333000000000;
+    // approx. $100 in wei at 1 ETH = $450
+    uint256 public constant PURCHASE_MIN_CAP_WEI = 222222222000000000;
 
     // approx. $15,000 in wei, per contributor
-    uint256 public constant PURCHASE_MAX_CAP_WEI = 50000000000000000000;
+    uint256 public constant PURCHASE_MAX_CAP_WEI = 33333333333000000000;
 
     // approx 49.5%
-    uint256 public constant FOUNDATION_POOL_TOKENS = 4949999505 * QUINTILLION;
+    uint256 public constant FOUNDATION_POOL_TOKENS = 2970000000 * MIN_UNIT;
 
     // 1%
-    uint256 public constant LEGAL_EXPENSES_TOKENS = 99999990 * QUINTILLION;
+    uint256 public constant LEGAL_EXPENSES_TOKENS = 60000000 * MIN_UNIT;
 
     // 16.5%
-    uint256 public constant FOUNDERS_TOKENS_VESTED = 1649999835 * QUINTILLION;
+    uint256 public constant FOUNDERS_TOKENS_VESTED = 990000000 * MIN_UNIT;
 }
